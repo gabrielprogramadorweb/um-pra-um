435f821ead15c6a1d58eaf9ae448b6e40ea0328c
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-14 23:28:51 -0300

    Sua mensagem de commit aqui

--- database/migrations/2023_12_15_021711_create_clientes_table.php
+++ database/migrations/2023_12_15_021711_create_clientes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class NomeDaMigracao extends Migration
+class CreateClientesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class NomeDaMigracao extends Migration
      */
     public function up()
     {
-        
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('telefone');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class NomeDaMigracao extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('clientes');
     }
 }
---
+++ database/migrations/2023_12_15_021752_create_enderecos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->string('rua');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('uf');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
