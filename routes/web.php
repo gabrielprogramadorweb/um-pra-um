53e91505d8181b12b8eba48201a29e467dbd5a86
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-15 00:07:32 -0300

    att

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
+use App\Cliente;
+use App\Endereco;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/clientes', function(){
+    $clientes = Cliente::all();
+    foreach($clientes as $c) {
+        echo "<p>ID: " . $c->id . "</p>";
+        echo "<p>Nome: " . $c->nome . "</p>";
+        echo "<p>Telefone: " . $c->telefone . "</p>";
+        echo "<hr>";
+
+    }
+    
+});
+
+Route::get('/enderecos', function(){
+    $ends = Endereco::all();
+    foreach($ends as $e) {
+        echo "<p>ID cliente: " . $e->cliente_id . "</p>";
+        echo "<p>Rua: " . $e->rua . "</p>";
+        echo "<p>Numero: " . $e->numero . "</p>";
+        echo "<p>Bairro: " . $e->bairro . "</p>";
+        echo "<p>Cidade: " . $e->cidade . "</p>";
+        echo "<p>UF" . $e->uf . "</p>";
+        echo "<p>CEP: " . $e->cep . "</p>";
+        echo "<hr>";
+
+    }
+    
 });
